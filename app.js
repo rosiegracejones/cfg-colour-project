7e92ecd9cc362435bc8d844dd6f8c20bea65f1c4
rosiegracejones <rosiegracejones@gmail.com>
2022-10-24 01:08:22 +0100

    Basic File Upload
    Changes added by SC - JS, CSS & HTML

---
+++ app.js
@@ -0,0 +1,15 @@
+function sayHello(event) {
+  event.preventDefault();
+
+  var childName = document.querySelector("#name-input").value;
+
+  if (childName !== "") {
+    var welcomeText = `Hello ${childName}!`;
+  } else {
+    var welcomeText = `Hello you!`;
+  }
+
+  document.getElementById("welcome").innerText = welcomeText;
+}
+
+document.addEventListener("submit", sayHello);
