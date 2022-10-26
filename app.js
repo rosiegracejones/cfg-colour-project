2261cf572f6537739ffb11751800619c5c4b3b7c
rosiegracejones <rosiegracejones@gmail.com>
2022-10-26 22:35:01 +0100

    styling & js
    updating font family, styles, buttons, layout and colours.
    
    Javascript now removes form when submitted and reveals option buttons.

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+  
 function sayHello(event) {
-  event.preventDefault();
-
+    event.preventDefault();
+  
   var childName = document.querySelector("#name-input").value;
-
+  
   if (childName !== "") {
-    var welcomeText = `Hello ${childName}!`;
-  } else {
-    var welcomeText = `Hello you!`;
+    var welcomeText = `🌈 
+    Hi ${childName}!`;
+  } 
+  
+  else {
+    var welcomeText = `Hi! 🌈`;
   }
-
+  
   document.getElementById("welcome").innerText = welcomeText;
+  document.getElementById("welcome").style.fontSize = '50px';
+  document.getElementById("name-submission").style.display = 'none';
+  document.getElementById("learn-mix-buttons").style.display = 'flex';
+  document.getElementById("choose-text").style.display = 'flex';
 }
 
-document.addEventListener("submit", sayHello);
-
+document.addEventListener("submit", sayHello)
