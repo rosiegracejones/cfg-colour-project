3f3c9e5f2bb841fc2c32902cdd081741183c7f11
rosiegracejones <rosiegracejones@gmail.com>
2022-10-28 00:30:55 +0100

    Just loads of stuff

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ function sayHello(event) {
   var childName = document.querySelector("#name-input").value;
   
   if (childName !== "") {
-    var welcomeText = `🌈 
-    Hi ${childName}!`;
+    var welcomeText = `🌈Hi ${childName}!`;
   } 
   
   else {
@@ -21,13 +20,56 @@ function sayHello(event) {
 
 document.addEventListener("submit", sayHello)
 
-function learnGreen(event) {
-  event.preventDefault();
+function learnOrange() {
 
-  document.getElementById("page-green").style.display = 'flex';
-  document.getElementsByClassName("learn-body").style.background-color = 'green';
-  document.getElementsByClassName("circle-container").style.display = 'none';
-
+  document.getElementById("circle-container").style.display = 'none'; 
+  document.getElementById("learn-body").style.backgroundColor = '#e87a22';
+  document.getElementById("page-orange").style.display = 'flex';
 }
 
-document.addEventListener("onClick()", learnGreen)
+function learnYellow() {
+
+  document.getElementById("circle-container").style.display = 'none'; 
+  document.getElementById("learn-body").style.backgroundColor = '#eecc0e';
+  document.getElementById("page-yellow").style.display = 'flex';
+}
+
+function learnGreen() {
+
+  document.getElementById("circle-container").style.display = 'none'; 
+  document.getElementById("learn-body").style.backgroundColor = '#00a61b';
+  document.getElementById("page-green").style.display = 'flex';
+}
+
+function learnBlue() {
+
+  document.getElementById("circle-container").style.display = 'none'; 
+  document.getElementById("learn-body").style.backgroundColor = '#3479f8';
+  document.getElementById("page-blue").style.display = 'flex';
+}
+
+function learnPurple() {
+
+  document.getElementById("circle-container").style.display = 'none'; 
+  document.getElementById("learn-body").style.backgroundColor = '#ac34f8';
+  document.getElementById("page-purple").style.display = 'flex';
+}
+
+function learnRed() {
+
+  document.getElementById("circle-container").style.display = 'none'; 
+  document.getElementById("learn-body").style.backgroundColor = '#ed1c24';
+  document.getElementById("page-red").style.display = 'flex';
+}
+
+function wheelReturn() {
+
+  document.getElementById("circle-container").style.display = 'block'; 
+  document.getElementById("learn-body").style.backgroundColor = '#e7def1';
+  document.getElementById("page-red").style.display = 'none';
+  document.getElementById("page-orange").style.display = 'none';
+  document.getElementById("page-yellow").style.display = 'none';
+  document.getElementById("page-green").style.display = 'none';
+  document.getElementById("page-blue").style.display = 'none';
+  document.getElementById("page-purple").style.display = 'none';
+}
